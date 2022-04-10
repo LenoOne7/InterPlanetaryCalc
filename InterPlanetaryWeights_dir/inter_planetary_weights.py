74decfc6e4bd8b68bd6090d128b7587ef425665d
Lennox Stampp <38123630+LenoOne7@users.noreply.github.com>
2022-04-10 17:13:47 -0400

    Final submitted version
    Room for lots of improvement.

--- InterPlanetaryWeights_dir/inter_planetary_weights.py
+++ InterPlanetaryWeights_dir/inter_planetary_weights.py
@@ -6,7 +6,7 @@
 #               factor on the surface of the planet."
 
 import tkinter as tk
-from tkinter import PhotoImage
+from tkinter import PhotoImage, messagebox
 
 # Variables
 nDICTCNVRSNFCTR = {
@@ -28,12 +28,6 @@ aUserInfo = ["", 0]  # index pos 0 = username | pos 1 = weight
 
 # Functions
 
-# ---Test Funcs---
-def ckVars(*args):
-    for arg in args:
-        print(arg)
-
-
 # Set sName and nEarthWght vars from entry fields
 def setNmWtVars(name_entry, wt_entry):
     try:
@@ -41,7 +35,7 @@ def setNmWtVars(name_entry, wt_entry):
         aUserInfo[1] = float(wt_entry.get())
     except:
         # --TODO-- Disallow submit to be pressed if entry invalid
-        pass
+        tk.messagebox.showinfo("Bad entry data", "Please exit program and reopen. Enter numbers only for weight.")
 
 
 def calcWeights():
@@ -50,17 +44,16 @@ def calcWeights():
 
 
 # --TODO--#
-# Fuction to Clear entries and reset variables
+# Fuction to Clear entries and reset variables to allow for retentry of data on intro window
 
 # GUI windows
 
 # --Main/Intro Window
 def introWin():
-    # Intro main window
+    # Intro/main window
     mainWindw = tk.Tk()
     mainWindw.title("Inter Planetary Weights GUI - By Lennox Stampp")
-    mainWindw.config(bg="black")  # #0d0d0d color is a shade of black
-    # main_win_width = mainWindw.winfo_screenwidth() #useless i think?? if so delete width from frames
+    mainWindw.config(bg="black")
 
     # Frames
     titleFrame = tk.Frame(mainWindw, bg="yellow", relief=tk.RIDGE, borderwidth=5)
@@ -70,6 +63,7 @@ def introWin():
     # Title frame widgets
     lblTitle = tk.Label(
         titleFrame,
+        font="bold",
         text="Inter Planetary Weights\nYour weight throughout the Solar System!",
         fg="yellow",
         bg="black",
@@ -90,10 +84,9 @@ def introWin():
     # --Submit button
     btnShowDispWin = tk.Button(buttonFrame,
                                text="Calculate Weights",
-                               command=lambda: [setNmWtVars(entNameEntryField, entWeightEntryField),
+                               command=lambda: [setNmWtVars(entNameEntryField,entWeightEntryField),
                                                 calcWeights(),
                                                 dispWin(),
-                                                ckVars(aUserInfo),
                                                 mainWindw.destroy()],
                                bg="yellow",
                                fg="black",
@@ -124,17 +117,14 @@ def introWin():
     btnCloseWin.pack(side="right")
 
 
-# --Result/Display windo
+# --Result/Display window
 def dispWin():
     # Variables
     sPlanetNamesIndxdList = [x for x in nDICTCNVRSNFCTR]
-    print(sPlanetNamesIndxdList)
     iIndxCurntPlnt = tk.IntVar()
     intIndxCurntPlnt = int(iIndxCurntPlnt.get())
-    # sCurrentPlanet = sPlanetNamesIndxdList[intIndxCurntPlnt]
     sCurrentPlanet = tk.StringVar()
     sCurrentPlanet.set(sPlanetNamesIndxdList[intIndxCurntPlnt])
-    print(sCurrentPlanet)
     sTitleStr = aUserInfo[0] + " on " + sCurrentPlanet.get()
 
     # Planet image dict
@@ -149,66 +139,72 @@ def dispWin():
                         "Uranus": "uranus_img2.png",
                         "Pluto" : "pluto_img2.png"
                         }
-    print(f"path for file = {imgPlanetImgDict[sCurrentPlanet.get()]}")
 
-    # imgCurentImg = tk.PhotoImage(file=imgPlanetImgDict[sCurrentPlanet])
-    # imgCurentImg = tk.PhotoImage(imgPlanetImgDict[sCurrentPlanet])
+    # dispWin Functions
 
-    # Functions
-
-    # Incremennt iIndxCurntPlnt
+    # Incremennt iIndxCurntPlnt and update screen
     def setNextIndx(event=None):
+        # If last image, go to first
         if iIndxCurntPlnt.get() == 9:
             iIndxCurntPlnt.set(0)
         else:
             iIndxCurntPlnt.set(iIndxCurntPlnt.get() + 1)
+        # Update variables and widgets
         sCurrentPlanet.set(sPlanetNamesIndxdList[iIndxCurntPlnt.get()])
+        # Heading
         sTitleStr = aUserInfo[0] + " on " + sCurrentPlanet.get()
         lblHeading.config(text=sTitleStr)
-        sDispData = f"{sTitleStr}:\nWeight on {sCurrentPlanet.get():<8}:{' . ' * 9:^12}{nIntrPlntWtDict[sCurrentPlanet.get()]:>10,.{2}f}lbs."
+        # Display data
+        sDispData = f"{sTitleStr}:\nWeight on {sCurrentPlanet.get():<8}:{' . ' * 9:^12}{nIntrPlntWtDict[sCurrentPlanet.get()]:>10,.{2}f} lbs."
         txtDispArea.delete("1.0", tk.END)
         txtDispArea.insert(tk.END, sDispData)
-        # txtDispArea.insert(tk.END, sCurrentPlanet.get())
+        # Create and update new image
         nextImg = tk.PhotoImage(master=lblPlanetImage, file=imgPlanetImgDict[sCurrentPlanet.get()])
         lblPlanetImage.image = nextImg
         lblPlanetImage.config(image=nextImg)
 
+    # Decrement iIndxCurntPlnt and update screen
     def setPrevIndx(event=None):
+        # Update variables and widgets
         iIndxCurntPlnt.set(iIndxCurntPlnt.get() - 1)
         sCurrentPlanet.set(sPlanetNamesIndxdList[iIndxCurntPlnt.get()])
+        # Heading
         sTitleStr = aUserInfo[0] + " on " + sCurrentPlanet.get()
         lblHeading.config(text=sTitleStr)
-        sDispData = f"{sTitleStr}:\nWeight on {sCurrentPlanet.get():<8}:{' . ' * 9:^12}{nIntrPlntWtDict[sCurrentPlanet.get()]:>10,.{2}f}lbs."
+        # Display data
+        sDispData = f"{sTitleStr}:\nWeight on {sCurrentPlanet.get():<8}:{' . ' * 9:^12}{nIntrPlntWtDict[sCurrentPlanet.get()]:>10,.{2}f} lbs."
         txtDispArea.delete("1.0", tk.END)
         txtDispArea.insert(tk.END, sDispData)
+        # Create and update new image
         nextImg = tk.PhotoImage(master=lblPlanetImage, file=imgPlanetImgDict[sCurrentPlanet.get()])
-
         lblPlanetImage.image = nextImg
         lblPlanetImage.config(image=nextImg)
 
+    # Show all weight and planets on one screen
     def listAllData(event=None):
+        # Heading
         sHeading = f"{aUserInfo[0]}, here are your weights\non our Solar System's planets"
         lblHeading.config(text=sHeading)
+        # Create and update new image
         nextImg = tk.PhotoImage(master=lblPlanetImage, file="solar_system_img2.png")
         lblPlanetImage.image = nextImg
         lblPlanetImage.config(image=nextImg)
+        # Display data
         txtDispArea.delete("1.0", tk.END)
-        sLine1 = f"{aUserInfo[0]}'s weight on:\n"
+        sLine1 = f"{aUserInfo[0]}'s weight on (scroll to view all):\n"
         txtDispArea.insert(tk.END, sLine1)
         for key, val in nIntrPlntWtDict.items():
-            data = f"\n\t{key:<8}:{' . ' * 5:^12}{val:>8,.{2}f}lbs.\n"
+            data = f"\n\t{key:<8}:{' . ' * 5:^12}{val:>8,.{2}f} lbs.\n"
             txtDispArea.insert(tk.END, data)
 
     # Display window
     dispWinw = tk.Tk()
     dispWinw.title("Inter Planetary Weights GUI - By Lennox Stampp")
     dispWinw.config(bg="yellow", highlightbackground="yellow", highlightcolor="yellow", highlightthickness=10)
-    #dispWin.geometry('1000x1000')
     winWidth = dispWinw.winfo_screenwidth()
     winHeight = dispWinw.winfo_screenheight()
-    # nWidth = int(winWidth/10)
-    nWidth = 42
-    nheight = 10
+    nWidth = 42     # Use for setting screen/widget width
+    nheight = 10    # Use for setting screen/widget height
 
     # Frames
     titleFrame = tk.Frame(dispWinw, bg="black", width=nWidth, highlightbackground="yellow", highlightcolor="yellow",
@@ -220,19 +216,17 @@ def dispWin():
     buttonFrame = tk.Frame(dispWinw, width=nWidth, bg="black")
 
     # Title frame widget
-    lblHeading = tk.Label(titleFrame,
-                          text=sTitleStr, bg="black", fg="yellow", width=nWidth)
+    lblHeading = tk.Label(titleFrame,font="bold", text=sTitleStr, bg="black", fg="yellow", width=nWidth)
 
     # Image frame widget
     lblPlanetImage = tk.Label(imageFrame)
-    # --TODO-- add try/escape for loading image. in escape, if image doesn't load, just add text of plnt name'
+    # --TODO-- add try/except for loading image. In except block, if image doesn't load, just add text of plnt name'
     imgCurentImg = tk.PhotoImage(master=lblPlanetImage, file=imgPlanetImgDict[sCurrentPlanet.get()])
-    # imgCurentImg = tk.PhotoImage(master=lblPlanetImage, file="mercury_img_png.png")
     lblPlanetImage.image = imgCurentImg
     lblPlanetImage.config(image=imgCurentImg)
 
     # Data display frame widget
-    sDispData = f"{sTitleStr}:\nWeight on {sCurrentPlanet.get():<8}:{'. ' * 9:^12}{nIntrPlntWtDict[sCurrentPlanet.get()]:>10,.{2}f}lbs."
+    sDispData = f"{sTitleStr}:\nWeight on {sCurrentPlanet.get():<8}:{'. ' * 9:^12}{nIntrPlntWtDict[sCurrentPlanet.get()]:>10,.{2}f} lbs."
     txtDispArea = tk.Text(dataDispFrame, bg="black", fg="yellow", width=nWidth, height=nheight)
     txtDispArea.insert(tk.END, sDispData)
 
